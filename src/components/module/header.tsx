4f093acb771076cf628f35abca4904f7759dfdc5
mohamad <mohamadshiravi85@gmail.com>
2025-04-09 21:54:39 +0330

    categories completed

--- src/components/module/header.tsx
+++ src/components/module/header.tsx
@@ -8,29 +8,8 @@ import CategoriesType from "../../types/categories";
 import { IoIosArrowBack } from "react-icons/io";
 
 export default function Header() {
-  const [hoverMenu, setHoverMenu] = useState<CategoriesType | false>(false);
+  const [hoverMenu, setHoverMenu] = useState<CategoriesType | false>();
   const [categories, setCategories] = useState<CategoriesType[]>([]);
-
-  // {
-  //   banner: "http://127.0.0.1:8000/media/products/categor",
-  //   id: 1,
-  //   title: "مردانه",
-  //   slug: "مردانه",
-  //   subcategories: [
-  //     {
-  //       banner: "http://127.0.0.1:8000/media/products/categor",
-  //       id: 1,
-  //       title: "مردانه",
-  //       slug: "مردانه",
-  //     },
-  //     {
-  //       banner: "http://127.0.0.1:8000/media/products/categor",
-  //       id: 1,
-  //       title: "مردانه",
-  //       slug: "مردانه",
-  //     },
-  //   ],
-  // }
 
   useEffect(() => {
     fetchCategories();
@@ -52,26 +31,36 @@ export default function Header() {
         <section className="w-full h-screen bg-black/20 backdrop-blur-[2px] fixed top-0 left-0 z-40"></section>
       )}
       {hoverMenu && (
-        <section className="px-10 py-4 rounded-b-lg w-[96%] left-[2%] h-[600px] fixed top-[160px] bg-white z-50">
+        <section
+          onMouseLeave={() => setHoverMenu(false)}
+          className="p-10 rounded-b-lg w-[96%] left-[2%] fixed top-[160px] bg-white z-50"
+        >
           <h3 className="text-gray-400 vazir-light text-sm underline underline-offset-[3px] cursor-pointer">
             همه محصولات {hoverMenu.title}
           </h3>
-          <div className="mt-10 flex gap-8">
-            {hoverMenu.subcategories.map((e) => (
-              <div>
-                <h4 className="flex items-center gap-2">
-                  <span className="bg-red-600 w-[5px] aspect-square block"></span>
-                  <span className="vazir-bold">{e.title}</span>
-                  <IoIosArrowBack />
-                </h4>
-                <div className="flex flex-col gap-1 mt-2 text-zinc-500">
-                  <span>زیر مجموعه</span>
-                  <span>زیر مجموعه</span>
-                  <span>زیر مجموعه</span>
-                  <span>زیر مجموعه</span>
+          <div className="flex justify-between">
+            <div className="mt-10 flex gap-8 ">
+              {hoverMenu.subcategories.map((e) => (
+                <div>
+                  <h4 className="flex items-center gap-2">
+                    <span className="bg-red-600 w-[5px] aspect-square block"></span>
+                    <span className="vazir-bold">{e.title}</span>
+                    <IoIosArrowBack />
+                  </h4>
+                  <div className="flex flex-col gap-1 mt-2 text-zinc-500">
+                    <span>زیر مجموعه</span>
+                    <span>زیر مجموعه</span>
+                    <span>زیر مجموعه</span>
+                    <span>زیر مجموعه</span>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
+            <img
+              src={hoverMenu.banner}
+              alt={hoverMenu.title}
+              className="max-w-[300px] aspect-square rounded-lg"
+            />
           </div>
         </section>
       )}
@@ -101,7 +90,6 @@ export default function Header() {
           {categories.map((e) => (
             <span
               onMouseEnter={() => setHoverMenu(e)}
-              onMouseLeave={() => setHoverMenu(false)}
               key={e.id}
               className="cursor-pointer after:transition-all px-4 after:content-[''] relative after:h-[2px] after:w-[0%] hover:after:w-[100%] after:bg-zinc-800 after:absolute after:-bottom-[13px] after:-right-0"
             >
