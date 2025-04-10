9620d4db0b2e48cdddab14829410d8230e2fccf6
mohamad <mohamadshiravi85@gmail.com>
2025-04-10 21:07:54 +0330

    categories section compeleted

--- src/components/template/home.tsx
+++ src/components/template/home.tsx
@@ -26,26 +26,51 @@ export default function Home() {
         ))}
       </Swiper>
 
-      <section className="lg:w-[1024px] w-full m-auto h-[2000px] mt-16">
-        <div className="flex items-center justify-center gap-10 w-full flex-wrap">
+      <section className="lg:w-[1024px] w-full m-auto h-[2000px] grid lg:mt-16 mt-6 overflow-hidden lg:px-0 px-2">
+        <Swiper
+          breakpoints={{
+            0: {
+              slidesPerView: 2.5,
+            },
+            450: {
+              slidesPerView: 3.5,
+            },
+            640: {
+              slidesPerView: 5,
+            },
+            768: {
+              slidesPerView: 6,
+            },
+            1024: {
+              slidesPerView: 8,
+            },
+          }}
+          className="w-full"
+        >
           {loading
-            ? Array.from({ length: 6 }).map((_) => (
-                <div className="flex flex-col items-center gap-3">
-                  <div className="rounded-full w-[120px] h-[120px] bg-zinc-200"></div>
-                  <div className="w-[80px] h-[25px] bg-zinc-200"></div>
-                </div>
+            ? Array.from({ length: 8 }).map((_) => (
+                <SwiperSlide className="lg:mx-2">
+                  <div className="flex flex-col items-center gap-3">
+                    <div className="rounded-full sm:w-[120px] w-[90px] sm:h-[120px] h-[90px] bg-zinc-200"></div>
+                    <div className="sm:w-[80px] w-[70px] sm:h-[25px] h-[20px] bg-zinc-200"></div>
+                  </div>
+                </SwiperSlide>
               ))
             : data?.map((e) => (
-                <div className="flex flex-col items-center gap-3">
-                  <img
-                    className="rounded-full w-[120px] h-[120px] object-cover"
-                    src={e.banner}
-                    alt={e.title}
-                  />
-                  <span className="vazir-bold">{e.title}</span>
-                </div>
+                <SwiperSlide className="lg:mx-2">
+                  <div className="flex flex-col items-center gap-3">
+                    <img
+                      className="rounded-full sm:w-[120px] w-[90] sm:h-[120px] h-[90px] object-cover"
+                      src={e.banner}
+                      alt={e.title}
+                    />
+                    <span className="vazir-bold text-center sm:text-base text-sm">
+                      {e.title}
+                    </span>
+                  </div>
+                </SwiperSlide>
               ))}
-        </div>
+        </Swiper>
       </section>
     </section>
   );
