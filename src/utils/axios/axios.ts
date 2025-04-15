d02ad7782e3c939b3ac5b38f04a443a863ec92f2
mohamad <mohamadshiravi85@gmail.com>
2025-04-15 14:47:35 +0330

    Change api url

--- src/utils/axios/axios.ts
+++ src/utils/axios/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://194.5.188.71/",
+  baseURL: "https://hoseinkocholoo-backend.onrender.com/",
   timeout: 10000,
   headers: {
     "Content-Type": "aplication/json",
